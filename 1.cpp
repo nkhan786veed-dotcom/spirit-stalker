9d3e95009c7aacf46e4fee743e019f3b2333acda
nkhan786veed-dotcom <nkhan786veed@gmail.com>
2025-12-04 12:30:39 -0800

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,1 +1,436 @@
+                   //1
 #include<iostream>
+using namespace std
+int main()
+{
+cout<<"HELLO WORLD"//body of program return 0;
+}
+                  //2
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"HELLO WORLD"//body of program return 0;
+}
+                   //3
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                    //4
+#include<iostream>
+usuing namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                    //5
+#include<iostream>
+using namespace std
+int main()
+{
+cout<<"hello world"//body of program return 0;  
+}
+                   //6
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                   //7
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                    //8
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                       //9
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                      //10
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                       //11  
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                        //12
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                       //13
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                        //14
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                       //15
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                       //16
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                      //17
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                     //18
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                      //19
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                    //20
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                  //21
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                   //22
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                   //23  
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                    //24  
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                      //25
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                       //26
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                         //27
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                       //28
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                         //29
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                          //30
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                           //31 
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                        //32
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                         //33
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                        //34
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                           //34
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                          //35
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                              //36
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                       //37
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                          //38
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                          //39
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                           //40
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                          //41
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                         //42
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                       //43
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                        //44
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                       //45    
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                           //46
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                           //47
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                           //48
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                           //49
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                           //50
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                          //51
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                        //52
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                         //53      
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                         //53
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                      //54
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                           //55 
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                        //56
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                                //57
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                      //58
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                      //59
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                      //60
+#include<iostream>
+using namespace std
+int main()
+{
+  cout<<"hello world"//body of program return 0;
+}
+                        //61
+          
